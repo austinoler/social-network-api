0997b1526d11d82f60fa91379455958ebc6c6f55
Austin Oler <oleraustin@gmail.com>
2024-01-19 14:05:25 -0500

    user route

---
+++ routes/user.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend
+} = require('../controllers/user-controller');
+
+// Define routes
+router.get('/', getAllUsers);
+router.get('/:id', getUserById);
+router.post('/', createUser);
+router.put('/:id', updateUser);
+router.delete('/:id', deleteUser);
+router.post('/:userId/friends/:friendId', addFriend);
+router.delete('/:userId/friends/:friendId', removeFriend);
+
+module.exports = router;
