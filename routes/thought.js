b21edeb0fcdb37cc49029ba5d8057a1736dc242b
Austin Oler <oleraustin@gmail.com>
2024-01-19 14:06:46 -0500

    thought route

---
+++ routes/thought.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+  getAllThoughts,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction
+} = require('../controllers/thought-controller');
+
+// Define routes
+router.get('/', getAllThoughts);
+router.get('/:id', getThoughtById);
+router.post('/', createThought);
+router.put('/:id', updateThought);
+router.delete('/:id', deleteThought);
+router.post('/:thoughtId/reactions', createReaction);
+router.delete('/:thoughtId/reactions/:reactionId', deleteReaction);
+
+module.exports = router;
